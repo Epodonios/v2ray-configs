8beaf08a2de3891c7bc5c63699c28ce3b610952a
Epodonios <158993328+Epodonios@users.noreply.github.com>
2024-02-09 19:53:10 +0330

    Create Replace.py

---
+++ Replace.py
@@ -0,0 +1,12 @@
+# باز کردن فایل برای خواندن
+with open('All_Configs_Sub.txt', 'r') as file:
+    file_content = file.read()
+
+# جایگزینی همه وقوع‌های 'Outline_vpn@' با 'epodonios'
+file_content = file_content.replace('Outline_vpn@', 'epodonios')
+
+# باز کردن فایل برای نوشتن و ذخیره محتوای تغییر یافته
+with open('All_Configs_Sub.txt', 'w') as file:
+    file.write(file_content)
+
+print("جایگزینی انجام شد و فایل با موفقیت ذخیره شد.")
