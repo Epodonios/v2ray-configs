58f537ce8c3e0f2afd0ecab4dbcb5e025d472b22
Epodonios <158993328+Epodonios@users.noreply.github.com>
2024-02-09 19:47:13 +0330

    Update sort.py

--- Files/sort.py
+++ Files/sort.py
@@ -20,7 +20,7 @@ vless = ""
 trojan = ""
 ss = ""
 ssr = ""
-respnse = requests.get("https://raw.githubusercontent.com/barry-far/V2ray-Configs/main/All_Configs_Sub.txt").text
+respnse = requests.get("https://raw.githubusercontent.com/Epodonios/v2ray-configs/main/All_Configs_Sub.txt").text
 for config in respnse.splitlines():
     if config.startswith("vmess"):
         open(vmess_file, "a").write(config + "\n")     
