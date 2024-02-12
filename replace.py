6eae1aa98a0bb522e7fc275bb66d9b36cf3ef986
ᛖᛈᛟᛞᛟᚾᛁᛟᛊ <epodonios@gmail.com>
2024-02-12 11:31:56 +0330

    Update replace.py

--- replace.py
+++ replace.py
@@ -1,10 +1,8 @@
-# Open the file Sub1.txt in read mode
 with open('All_Configs_Sub.txt', 'r') as file:
-    data = file.read()
+    content = file.read()
 
-# Replace all occurrences of 'Eliv2ray' with 'Hello'
-data = data.replace('Eliv2ray', 'Hello')
+content = content.replace('Eliv2ray', 'Hello')
+content = content.replace('ipv2ray', 'Hello')
 
-# Open the file Sub1.txt in write mode and write the modified data
 with open('All_Configs_Sub.txt', 'w') as file:
-    file.write(data)
+    file.write(content)
