381e345db36de8f2cd77136177c2d2bd8188d6a3
ᛖᛈᛟᛞᛟᚾᛁᛟᛊ <epodonios@gmail.com>
2024-02-12 11:44:42 +0330

    Update replace.py

--- replace.py
+++ replace.py
@@ -1,8 +1,14 @@
-with open('All_Configs_Sub.txt', 'r') as file:
+file_path = "Sub7.txt"
+
+# باز کردن فایل و خواندن محتوا
+with open(file_path, "r") as file:
     content = file.read()
 
-content = content.replace('Eliv2ray', 'Hello')
-content = content.replace('ipv2ray', 'Hello')
+# جایگزینی همه کلمات ipv2ray با mmd
+new_content = content.replace("ipv2ray", "mmd")
 
-with open('All_Configs_Sub.txt', 'w') as file:
-    file.write(content)
+# آپدیت فایل با محتوای جدید
+with open(file_path, "w") as file:
+    file.write(new_content)
+
+print("همه کلمات 'ipv2ray' در فایل جایگزین شدند.")
